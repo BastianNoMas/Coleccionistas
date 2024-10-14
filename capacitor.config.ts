94ce61b0d7bb7d932e46dac807e70e2cfbb1f9a8
BastianNoMas <bastian.ihh@gmail.com>
2024-10-13 23:38:10 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'coleccionistas',
+  webDir: 'www'
+};
+
+export default config;
