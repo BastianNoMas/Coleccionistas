37ec090e3522c57b423915c10e9ca198cddb0e19
FranciscoDiazPinto <137012203+FranciscoDiazPinto@users.noreply.github.com>
2024-10-14 21:46:24 -0300

    menu, intento

--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -3,14 +3,40 @@ import { IonicModule } from '@ionic/angular';
 
 import { MenuComponent } from './menu.component';
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+
 describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
+
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MenuComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+  });
+
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  
+
+
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MenuComponent ],
       imports: [IonicModule.forRoot()]
+      
     }).compileComponents();
 
     fixture = TestBed.createComponent(MenuComponent);
@@ -22,3 +48,15 @@ describe('MenuComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+
+
+
+
+
+
+
+
+  
+
+ 
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -12,3 +12,6 @@ export class MenuComponent  implements OnInit {
   ngOnInit() {}
 
 }
+
+
+
